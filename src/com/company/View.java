4166cf6af588a7f45636bbfd4df9b1273df88185
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-11-20 22:27:22 +0200

    view implemented

--- src/com/company/View.java
+++ src/com/company/View.java
@@ -1,6 +1,47 @@
 package com.company;
 
+import com.company.controller.Controller;
+import com.company.model.Ball;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class View extends JPanel {
+    private static final Color BALL_COLOR = new Color(0x00ffbb);
+
+
+    private Controller controller;
+
+    public View(Controller controller) {
+        setFocusable(true);
+        this.controller = controller;
+        addKeyListener(controller);
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 0, this.getSize().width, this.getSize().height);
+        drawField(g);
+        drawPlayer(g);
+    }
+
+    private void drawField(Graphics g2) {
+        g2.setColor(Color.BLACK);
+        g2.drawRect(1, 1, controller.getField().getWidth(), controller.getField().getHeight());
+    }
+
+    private void drawPlayer(Graphics g2) {
+        g2.setColor(BALL_COLOR);
+
+        Ball ball = controller.getPlayer();
+
+        g2.fillOval(ball.getX() - ball.getRadius(), ball.getY() - ball.getRadius(), ball.getRadius() * 2, ball.getRadius() * 2);
+
+        g2.setColor(Color.BLACK);
+
+        g2.drawOval(ball.getX() - ball.getRadius(), ball.getY() - ball.getRadius(), ball.getRadius() * 2, ball.getRadius() * 2);
+    }
+
 }
