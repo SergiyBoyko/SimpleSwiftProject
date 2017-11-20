1006ce63b6712d9c9a30030b767a6518220124c3
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-11-20 22:28:09 +0200

    main class implemented

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,4 +1,25 @@
 package com.company;
 
+import com.company.controller.Controller;
+
+import javax.swing.*;
+
 public class Main {
+    public static void main(String[] args) {
+        Controller controller = new Controller();
+
+        JFrame frame = new JFrame();
+        frame.setTitle("Simple swing project");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(600, 500);
+        frame.setResizable(false);
+
+        frame.add(controller.getView());
+
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+        controller.startGame();
+
+    }
 }
